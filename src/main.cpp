87ed72d176da4c7c2ac8f4e2b36673c5a7ea5c49
syh <13951968938@126.com>
2024-04-04 00:39:32 +0800

    test asio

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,56 @@
 #include "../include/SDLMic.h"
 #include <thread>
 #include <asio.hpp>
+using asio::ip::tcp;
+
+class TCPServer
+{
+private:
+    asio::io_context io_context;
+    tcp::acceptor acceptor;
+    tcp::socket socket;
+    std::vector<int> received_array;
+
+public:
+    TCPServer(int port)
+        : acceptor(io_context, tcp::endpoint(tcp::v4(), port)),
+          socket(io_context)
+    {
+
+        acceptor.accept(socket);
+        std::cout << "nihao";
+        int array_size;
+        asio::read(socket, asio::buffer(&array_size, sizeof(array_size)));
+
+        received_array.resize(array_size);
+        asio::read(socket, asio::buffer(received_array));
+    }
+
+    std::vector<int> receiveArray()
+    {
+        return received_array;
+    }
+};
+
+class TCPClient
+{
+private:
+    asio::io_context io_context;
+    tcp::socket socket;
+
+public:
+    TCPClient(const std::string &ip, int port)
+        : socket(io_context)
+    {
+        tcp::resolver resolver(io_context);
+        asio::connect(socket, resolver.resolve(ip, std::to_string(port)));
+
+        std::vector<int> arrayToSend = {1, 2, 3, 4, 5};
+        int array_size = arrayToSend.size();
+        asio::write(socket, asio::buffer(&array_size, sizeof(array_size)));
+        asio::write(socket, asio::buffer(arrayToSend));
+    }
+};
 void recordWithMic(SDLMic *sdlmic)
 {
     sdlmic->startRecording();
@@ -16,8 +66,23 @@ void playWithSpeaker(SDLSpeaker *sdlspeaker)
 }
 int main2()
 {
-    auto &&service = asio::io_service{};
-    (void)service;
+    const int port = 8080;
+    std::thread server_thread([&]()
+                              {
+        TCPServer server(port);
+        std::vector<int> receivedArray = server.receiveArray();
+        std::cout << "Received array: ";
+        for (int num : receivedArray) {
+            std::cout << num << " ";
+        }
+        std::cout << std::endl; });
+
+    std::thread client_thread([&]()
+                              { TCPClient client("127.0.0.1", port); });
+
+    server_thread.join();
+    client_thread.join();
+
     return 0;
 }
 int main1()
@@ -67,5 +132,5 @@ int main1()
 }
 int main(int argc, char *args[])
 {
-    return main1();
+    return main2();
 }

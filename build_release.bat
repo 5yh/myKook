5c7f299a657db0756bae2f897d71c7a16db4f601
syh <13951968938@126.com>
2024-04-06 03:44:11 +0800

    delete zf

--- build_release.bat
+++ build_release.bat
@@ -1,3 +1,3 @@
 conan install . --profile="conan_profile_win64_release"  --output-folder=build-release --build=missing --remote=conancenter
 cd build-release
-cmake .. -G "Visual Studio 17 2022" -DZFG_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE="conan_toolchain.cmake"
+cmake .. -G "Visual Studio 17 2022" -DCMAKE_TOOLCHAIN_FILE="conan_toolchain.cmake"

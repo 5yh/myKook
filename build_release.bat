d8dfc10cce8cade4739570e07eb9a48dccf66a62
syh <13951968938@126.com>
2024-04-07 14:50:44 +0800

    change file structure

--- build_release.bat
+++
@@ -1,3 +0,0 @@
-conan install . --profile="conan_profile_win64_release"  --output-folder=build-release --build=missing --remote=conancenter
-cd build-release
-cmake .. -G "Visual Studio 17 2022" -DCMAKE_TOOLCHAIN_FILE="conan_toolchain.cmake"

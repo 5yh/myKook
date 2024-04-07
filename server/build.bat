77a0c9cbbdb3ddc863914406515decf9e8f4738c
suyuhao <13951968938@126.com>
2024-04-08 00:35:46 +0800

    add server

---
+++ server/build.bat
@@ -0,0 +1,3 @@
+conan install . --profile="conan_profile_win64_debug"  --output-folder=build-debug --build=missing --remote=conancenter
+cd build-debug
+cmake .. -G "Visual Studio 17 2022" -DCMAKE_TOOLCHAIN_FILE="conan_toolchain.cmake"

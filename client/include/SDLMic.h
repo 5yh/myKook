8bb3447d1872c38e87c4bbb38b513e09e918349c
syh <13951968938@126.com>
2024-04-07 14:50:25 +0800

    add client subfolder change file structure

---
+++ client/include/SDLMic.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+#pragma execution_character_set("utf-8")
+#include <SDL.h>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include "SDLSpeaker.h" // Assuming the header file is in the same directory
+#include "../src/net/netTest.cpp"
+#include <asio.hpp>
+class SDLMic
+{
+public:
+    SDLMic();
+
+    void showMicDevices();
+    void showMicDevice(int index);
+    void chooseMicDevice();
+    std::string getChosenDeviceName();
+    void setDesiredSpec(int sampleRate = 44100, int framesPerBuffer = 512);
+    void initAudioDevice();
+    void startRecording();
+    void stopRecording();
+    void startSaveWav();
+    void stopSaveWav();
+    SDL_AudioDeviceID getMicID();
+    void setSpeaker(SDLSpeaker &speaker);
+    void setClient(Client &client);
+
+private:
+    int micNumber;
+    int micIndex;
+    std::string micDeviceName;
+    SDL_AudioSpec desiredSpec, obtainedSpec;
+    SDL_AudioDeviceID micDevice;
+    SDLSpeaker *speaker;
+    std::ofstream audioFile;
+    // client的引用
+    Client *client;
+    static void audioCallback3(void *userdata, Uint8 *stream, int len);
+    static void audioCallback2(void *userdata, Uint8 *stream, int len);
+    static void audioCallback(void *userdata, Uint8 *stream, int len);
+    void writeWavHeader(std::ofstream &file, int sampleRate, int numChannels, int bitsPerSample);
+};
---
+++ client/src/main.cpp
@@ -0,0 +1,229 @@
+﻿#include <SDL.h>
+#include <iostream>
+#include <locale.h>
+#include "../include/SDLSpeaker.h"
+#include "../include/SDLMic.h"
+#include <thread>
+#include "net\netTest.cpp"
+#include <chrono>
+using asio::ip::tcp;
+void recordWithMic(SDLMic *sdlmic)
+{
+    sdlmic->startRecording();
+}
+
+void playWithSpeaker(SDLSpeaker *sdlspeaker)
+{
+    sdlspeaker->startPlayBack();
+}
+int main4()
+{
+    setlocale(LC_ALL, ".65001"); // 设置当前区域为UTF-8
+    SDL_version v;
+    SDL_GetVersion(&v);
+    std::cout << "SDL version " << int(v.major) << "." << int(v.minor) << "." << int(v.patch) << std::endl;
+    // 初始化
+    if (SDL_Init(SDL_INIT_AUDIO) != 0)
+    {
+        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+        return 1;
+    }
+
+    std::cout << "---------------------------------------------------" << std::endl;
+    SDLSpeaker *sdlspeaker = new SDLSpeaker();
+    sdlspeaker->showSpeakerDevices();
+    sdlspeaker->chooseSpeakerDevice();
+    sdlspeaker->setDesiredSpec();
+    sdlspeaker->initAudioDevice();
+    sdlspeaker->startPlayBack();
+    try
+    {
+        asio::io_context io_context;
+        std::cout << "start server thread\n";
+        Server server(io_context, 18080, sdlspeaker);
+        io_context.run();
+
+        SDL_Delay(100000);
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+    SDL_Quit();
+    return 0;
+    // try
+    // {
+    //     asio::io_context io_context;
+
+    //     std::thread server_thread([&io_context, &sdlspeaker]()
+    //                               {
+    //                                 std::cout<<"start server thread\n";
+    //         Server server(io_context, 8080);
+    //         io_context.run();
+    //         sdlspeaker->startPlayBack(); });
+    //     SDL_Delay(100000);
+    //     server_thread.join();
+    // }
+    // catch (std::exception &e)
+    // {
+    //     std::cerr << "Exception: " << e.what() << std::endl;
+    // }
+
+    // 多线程停止
+}
+int main3()
+{
+    setlocale(LC_ALL, ".65001"); // 设置当前区域为UTF-8
+    SDL_version v;
+    SDL_GetVersion(&v);
+    std::cout << "SDL version " << int(v.major) << "." << int(v.minor) << "." << int(v.patch) << std::endl;
+    // 初始化
+    if (SDL_Init(SDL_INIT_AUDIO) != 0)
+    {
+        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+        return 1;
+    }
+    SDLMic *sdlmic = new SDLMic();
+    sdlmic->showMicDevices();
+    sdlmic->chooseMicDevice();
+    sdlmic->setDesiredSpec();
+    sdlmic->initAudioDevice();
+    try
+    {
+        asio::io_context io_context;
+        std::cout << "start client thread\n";
+        Client client(io_context, "127.0.0.1", "18080");
+        sdlmic->setClient(client);
+        sdlmic->startRecording();
+        SDL_Delay(100000);
+        std::cout << "111";
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << "myException: " << e.what() << std::endl;
+    }
+    sdlmic->stopRecording();
+
+    // SDL_Quit();
+    return 0;
+
+    // try
+    // {
+    //     asio::io_context io_context;
+    //     std::thread client_thread([&io_context, &sdlmic]()
+    //                               {
+    //                                   std::cout << "start client thread\n";
+    //                                   Client client(io_context, "127.0.0.1", "8080");
+    //                                   sdlmic->setClient(client);
+    //                                   sdlmic->startRecording(); });
+    //     SDL_Delay(20000);
+    //     std::cout << "111";
+    //     client_thread.join();
+    // }
+    // catch (std::exception &e)
+    // {
+    //     std::cerr << "myException: " << e.what() << std::endl;
+    // }
+    // 多线程
+    // std::thread recordThread(recordWithMic, sdlmic);
+    // std::thread playThread(playWithSpeaker, sdlspeaker);
+
+    // 多线程停止
+    // recordThread.join();
+    // sdlmic->stopSaveWav();
+}
+// int main2()
+// {
+//     // Server
+//     try
+//     {
+//         asio::io_context io_context;
+
+//         std::thread server_thread([&io_context]()
+//                                   {
+//                                     std::cout<<"start server thread\n";
+//             Server server(io_context, 8080);
+//             io_context.run(); });
+//         std::thread client_thread([&io_context]()
+//                                   {
+//                                       std::cout << "start client thread\n";
+//                                       Client client(io_context, "127.0.0.1", "8080");
+
+//                                       Uint8 data[stream_length];  // 假设有要发送的数据
+//                                       int length = stream_length; // 假设数据长度为 stream_length
+//                                       while (1)
+//                                       {
+//                                           client.send_data(data, length);
+//                                           std::this_thread::sleep_for(std::chrono::seconds(1));
+//                                       }
+
+//                                       //   std::cout<<"client after send\n";
+//                                   });
+//         server_thread.join();
+//         client_thread.join();
+//     }
+//     catch (std::exception &e)
+//     {
+//         std::cerr << "Exception: " << e.what() << std::endl;
+//     }
+
+//     return 0;
+// }
+int main1()
+{
+    setlocale(LC_ALL, ".65001"); // 设置当前区域为UTF-8
+    SDL_version v;
+    SDL_GetVersion(&v);
+    std::cout << "SDL version " << int(v.major) << "." << int(v.minor) << "." << int(v.patch) << std::endl;
+    // 初始化
+    if (SDL_Init(SDL_INIT_AUDIO) != 0)
+    {
+        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+        return 1;
+    }
+    SDLMic *sdlmic = new SDLMic();
+    sdlmic->showMicDevices();
+    // sdlmic->showMicDevice(0);
+    sdlmic->chooseMicDevice();
+    sdlmic->setDesiredSpec();
+    sdlmic->initAudioDevice();
+    // sdlmic->startRecording();
+    // sdlmic->startSaveWav();
+
+    std::cout << "---------------------------------------------------" << std::endl;
+    SDLSpeaker *sdlspeaker = new SDLSpeaker();
+    sdlspeaker->showSpeakerDevices();
+    // sdlspeaker->showSpeakerDevice(0);
+    sdlspeaker->chooseSpeakerDevice();
+    sdlspeaker->setDesiredSpec();
+    sdlspeaker->initAudioDevice();
+    // sdlspeaker->setMic(*sdlmic);
+    sdlmic->setSpeaker(*sdlspeaker);
+    // sdlspeaker->startPlayBack();
+    // 多线程
+    std::thread recordThread(recordWithMic, sdlmic);
+    std::thread playThread(playWithSpeaker, sdlspeaker);
+    SDL_Delay(10000);
+    // 多线程停止
+    playThread.join();
+    recordThread.join();
+    // sdlmic->stopSaveWav();
+    sdlspeaker->stopPlayBack();
+    sdlmic->stopRecording();
+
+    SDL_Quit();
+    return 0;
+}
+int main(int argc, char *args[])
+{
+    // return main1();
+    // return main2();
+    // std::thread thread1(main4);
+    // std::this_thread::sleep_for(std::chrono::seconds(5));
+    std::thread thread2(main3);
+
+    // thread1.join();
+    thread2.join();
+    // // main1();
+    return 0;
+}
